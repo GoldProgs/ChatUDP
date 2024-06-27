e68b4026e7a7f5d50a33a519eaa54ccdcddf854a
Kostya <emelyanov4512@gmail.com>
2024-06-28 00:48:04 +0300

    Создал интерфейс регистрации пользователя, а также добавил модель User.

---
+++ ChatUDP/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using ChatUDP.Models;
+using System.Configuration;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatUDP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void RegButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ChatUDP/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ChatUDP.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+#pragma warning disable CS8618 
+        [NotNull]
+        [MinLength(5, ErrorMessage = "Минимальная длина ФИО должна составлять 5 символов.")]
+        [MaxLength(160, ErrorMessage = "Максимальная длина ФИО должна составлять 160 символов.")]
+        public string FullName { get; set; }
+
+        [NotNull]
+        [MinLength(8, ErrorMessage = "Пароль должен содержать минимум 8 символов.")]
+        [MaxLength(128, ErrorMessage = "Максимальная длина пароля должна составлять 128 символов.")]
+        public string Password { get; set; }
+
+        [NotNull]
+        [MaxLength(256, ErrorMessage = "Максимальная длина электронной почты должна составлять 256 символов.")]
+        [EmailAddress(ErrorMessage = "Некорректно введённый адрес электронной почты или его формат.")]
+        public string Email { get; set; }
+
+        [NotNull]
+        public DateTime Birthday { get; set; }
+
+#pragma warning restore CS8618
+    }
+}
